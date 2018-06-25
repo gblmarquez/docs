dddbcc8c2daf015269b09b0d0a4f9c58d45ce5d6
yagosenhorini <yagosenhorini30@gmail.com>
2018-06-25 11:04:28 -0300

    Change page content

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -31,13 +31,13 @@ class Users extends React.Component {
         <Container padding={['bottom', 'top']}>
           <div className="showcaseSection">
             <div className="prose">
-              <h1>Who's Using This?</h1>
-              <p>This project is used by many folks</p>
+              <h1>Quem está usando isso?</h1>
+              <p>Este projeto é utilizado por várias pessoas</p>
             </div>
             <div className="logos">{showcase}</div>
-            <p>Are you using this project?</p>
+            <p>Você está usando esse projeto?</p>
             <a href={editUrl} className="button">
-              Add your company
+              Adicione sua empresa
             </a>
           </div>
         </Container>
