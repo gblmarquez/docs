0d78a002ef40d1cf4cb23f5fb23cbc0ca8fb026d
yagosenhorini <yagosenhorini30@gmail.com>
2018-07-04 10:54:14 -0300

    Change baseurl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -216,7 +216,7 @@ const siteConfig = {
   tagline: 'Documentações',
   title: 'NFe.io : Documentação' /* title for your website */,
   url: 'https://yagosenhorini.github.io' /* your website url */,
-  baseUrl: '/docs/' /* base url for your project */,
+  baseUrl: '/docusaurus/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
